1365ad6c5fcabf14aa5ccdfd31883905ab359525
talekunci <smouke.king1@gmail.com>
2023-12-16 21:36:04 +0200

    The third task has fixed.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,55 @@
-#include <iostream>
-#include "fourthTask/solution/Worker.cpp"
+#include "thirdTask/solution/Date.h"
+#include "thirdTask/solution/Competition.h"
+#include "thirdTask/solution/Show.h"
+#include "thirdTask/solution/ParticipantCompetition.h"
 
 int main() {
-    return Worker::main();
+    Competition c = Competition();
+    Competition c1 = Competition("Competition2");
+    Competition c2 = Competition("Competition3");
+
+    Date tmp = Date(1, 1, 1996);
+    Date d = Date();
+    Date d1 = Date(14, 12, 1998);
+    Date d2 = Date(tmp);
+
+    ParticipantCompetition pc = ParticipantCompetition("Mykola", "Lybid'", d);
+    ParticipantCompetition pc1 = ParticipantCompetition();
+    ParticipantCompetition pc2 = ParticipantCompetition("Roman", "Shpak", d2);
+
+    pc1.setBirthDate(d1);
+    pc1.setFirstName("James");
+    pc1.setLastName("Bay");
+
+    Show s = Show();
+    Show s1 = Show(true, pc, 5, "Win win", "Lose");
+    Show s2 = Show(false, pc1, 4, "Something", "Anything");
+
+    s.setParticipantCompetition(pc2);
+    s.setCount(8);
+    s.setGoal("Word");
+    s.setResult("Sequence");
+
+    c1.addShow(s1, "description");
+
+    c2.addShow(s, "Lorem");
+    c2.addShow(s2, "Ipsum");
+
+    c.printAll();
+    cout << "=============" << endl;
+    c.printShortly();
+
+    cout << "-----------------------" << endl;
+
+    c1.printAll();
+    cout << "=============" << endl;
+    c1.printShortly();
+
+    cout << "------------------------" << endl;
+
+    c2.printAll();
+    cout << "=============" << endl;
+    c2.printShortly();
+
+    return 0;
 }
--- thirdTask/solution/Show.h
+++ thirdTask/solution/Show.h
@@ -9,94 +9,97 @@
 #include "ParticipantCompetition.h"
 #include <ostream>
 
-class Show {
-private:
-    bool team;
-    static int count;
-    ParticipantCompetition participantCompetition;
-    int seqNumber = count;
-    string goal;
-    string result;
-public:
-    Show() {}
+namespace {
+    class Show {
+    private:
+        bool team;
+        static int count;
+        ParticipantCompetition participantCompetition;
+        int seqNumber = count;
+        string goal;
+        string result;
+    public:
+        Show() {}
 
-    Show(bool team, const ParticipantCompetition &participantCompetition, int seqNumber, const string &goal,
-         const string &result) : team(team), participantCompetition(participantCompetition), seqNumber(seqNumber),
-                                 goal(goal), result(result) {}
+        Show(bool team, const ParticipantCompetition &participantCompetition, int seqNumber, const string &goal,
+             const string &result) : team(team), participantCompetition(participantCompetition), seqNumber(seqNumber),
+                                     goal(goal), result(result) {}
 
-    virtual ~Show() {
+        virtual ~Show() {
 
-    }
+        }
 
-    bool getTeam() const {
-        return team;
-    }
+        bool getTeam() const {
+            return team;
+        }
 
-    void setTeam(bool team) {
-        Show::team = team;
-    }
+        void setTeam(bool team) {
+            Show::team = team;
+        }
 
-    static int getCount() {
-        return count;
-    }
+        static int getCount() {
+            return count;
+        }
 
-    static void setCount(int count) {
-        Show::count = count;
-    }
+        static void setCount(int count) {
+            Show::count = count;
+        }
 
-    const ParticipantCompetition &getParticipantCompetition() const {
-        return participantCompetition;
-    }
+        const ParticipantCompetition &getParticipantCompetition() const {
+            return participantCompetition;
+        }
 
-    void setParticipantCompetition(const ParticipantCompetition &participantCompetition) {
-        Show::participantCompetition = participantCompetition;
-    }
+        void setParticipantCompetition(const ParticipantCompetition &participantCompetition) {
+            Show::participantCompetition = participantCompetition;
+        }
 
-    int getSeqNumber() const {
-        return seqNumber;
-    }
+        int getSeqNumber() const {
+            return seqNumber;
+        }
 
-    void setSeqNumber(int seqNumber) {
-        Show::seqNumber = seqNumber;
-    }
+        void setSeqNumber(int seqNumber) {
+            Show::seqNumber = seqNumber;
+        }
 
-    const string &getGoal() const {
-        return goal;
-    }
+        const string &getGoal() const {
+            return goal;
+        }
 
-    void setGoal(const string &goal) {
-        Show::goal = goal;
-    }
+        void setGoal(const string &goal) {
+            Show::goal = goal;
+        }
 
-    const string &getResult() const {
-        return result;
-    }
+        const string &getResult() const {
+            return result;
+        }
 
-    void setResult(const string &result) {
-        Show::result = result;
-    }
+        void setResult(const string &result) {
+            Show::result = result;
+        }
 
-    inline bool operator< (const Show &show) const {
-        return seqNumber < show.getSeqNumber();
-    }
+        inline bool operator< (const Show &show) const {
+            return seqNumber < show.getSeqNumber();
+        }
 
-    void print() {
-        cout << "Is the show team: " << team << ";" << endl;
-        cout << "Participant Competition: ";
-        participantCompetition.print();
-        cout << ";" << endl;
-        cout << "Sequence number: " << seqNumber << ";" << endl;
-        cout << "Goal: " << goal << ";" << endl;
-        cout << "Result: " << result << ";" << endl;
-    }
+        void print() {
+            cout << "Is the show team: " << team << ";" << endl;
+            cout << "Participant Competition: ";
+            participantCompetition.print();
+            cout << ";" << endl;
+            cout << "Sequence number: " << seqNumber << ";" << endl;
+            cout << "Goal: " << goal << ";" << endl;
+            cout << "Result: " << result << ";" << endl;
+        }
 
-    friend ostream &operator<<(ostream &os, const Show &show) {
-        os << "team: " << show.team << " participantCompetition: " << show.participantCompetition << " seqNumber: "
-           << show.seqNumber << " goal: " << show.goal << " result: " << show.result;
-        return os;
-    }
+        friend ostream &operator<<(ostream &os, const Show &show) {
+            os << "team: " << show.team << " participantCompetition: " << show.participantCompetition << " seqNumber: "
+               << show.seqNumber << " goal: " << show.goal << " result: " << show.result;
+            return os;
+        }
 
-};
+    };
 
+int Show::count = 0;
+}
 
 #endif //CPP_SHOW_H
